eb3b4c03c116367c6b2f60d34e04afa3a4028291
Sami Hangaslammi <shang@iki.fi>
2011-03-15 19:52:19 +0200

    adjusted ship shape

--- Haskeroids/Player.hs
+++ Haskeroids/Player.hs
@@ -14,5 +14,5 @@ shipSize = 12.0
 shipLines :: [LineSegment]
 shipLines = linesToSegments points
     where points = [radial shipSize 0, radial shipSize (0.7*pi),
-                    radial (shipSize*0.5) pi, radial shipSize (1.3*pi),
+                    radial (shipSize*0.2) pi, radial shipSize (1.3*pi),
                     radial shipSize 0]
