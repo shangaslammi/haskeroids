71db0d8902061fd734dfb8e6598bf3de2282a08f
Sami Hangaslammi <shang@iki.fi>
2011-03-19 16:00:39 +0200

    comment tweaks

--- Haskeroids/Geometry/Body.hs
+++ Haskeroids/Geometry/Body.hs
@@ -23,17 +23,7 @@ data Body = Body {
 -- | Initialize a new rigid body in the given location
 initBody :: Vec2 -> Body
 initBody pos = Body pos 0 (0,0) 0
-    
--- | Accelerate a rigid body according to the given vector
-accelerate :: Vec2 -> Body -> Body
-accelerate (ax,ay) b = b { bodyVelocity = newVelocity }
-    where newVelocity = (ax+vx, ay+vy)
-          (vx,vy)     = bodyVelocity b
 
--- | Accelerate a rigid body according to its current angle
-accForward :: Float -> Body -> Body
-accForward m b = accelerate (polar m (bodyAngle b)) b
-          
 -- | Update the position and orientation of a body according to its current
 --   velocity and rotation.
 updateBody :: Body -> Body
@@ -41,6 +31,17 @@ updateBody b = b { bodyPos = pos, bodyAngle = a }
     where pos = bodyPos b   /+/ bodyVelocity b
           a   = bodyAngle b + bodyRotation b
 
+-- | Accelerate a rigid body with the given vector
+accelerate :: Vec2 -> Body -> Body
+accelerate (ax,ay) b = b { bodyVelocity = newVelocity }
+    where newVelocity = (ax+vx, ay+vy)
+          (vx,vy)     = bodyVelocity b
+
+-- | Accelerate a rigid body in the direction of its current angle with the
+--   given magnitude.
+accForward :: Float -> Body -> Body
+accForward m b = accelerate (polar m (bodyAngle b)) b
+          
 -- | Apply a damping effect to velocity using the given coefficient
 damping :: Float -> Body -> Body
 damping coefficient b = b { bodyVelocity = coefficient */ bodyVelocity b }
