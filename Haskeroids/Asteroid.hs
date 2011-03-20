d536c47eacb4701ebf5852f496c52ab2183b681f
Sami Hangaslammi <shang@iki.fi>
2011-03-20 09:34:05 +0200

    added comments

--- Haskeroids/Asteroid.hs
+++ Haskeroids/Asteroid.hs
@@ -14,16 +14,29 @@ data Asteroid = Asteroid Size Body
 instance LineRenderable Asteroid where
     interpolatedLines f (Asteroid sz b) = map (transform b') $ asteroidLines sz
         where b' = interpolatedBody f b
-    
+
+-- | Initialize a new asteroid with the given position, velocity and rotation        
 newAsteroid :: Vec2 -> Vec2 -> Float -> Asteroid
 newAsteroid pos v r = Asteroid Large $ Body pos 0 v r pos 0
 
+-- | Update an asteroid's position
 updateAsteroid :: Asteroid -> Asteroid
 updateAsteroid (Asteroid sz b) = Asteroid sz $ updateBody b
 
+-- | Get the radius for an asteroid size
+radius :: Size -> Float
+radius Small  = 14
+radius Medium = 28
+radius Large  = 56
+
+-- | Get the number of vertices for an asteroid size
+numVertices :: Size -> Int
+numVertices Small  = 6
+numVertices Medium = 8
+numVertices Large  = 12
+
+-- | Get the line segments for an asteroid size
 asteroidLines sz = pointsToSegments $ pts sz
-    where pts Small  = polarPoints 6 14
-          pts Medium = polarPoints 8 28
-          pts Large  = polarPoints 12 56
+    where pts sz  = polarPoints (numVertices sz) (radius sz)
           polarPoints s r = map (polar r) [0.0,step..2.0*pi]
              where step = 2.0*pi/s
