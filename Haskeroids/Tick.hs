95bc4ae458033272406e9bf7b9e0ee035c0f87ec
Sami Hangaslammi <shang@iki.fi>
2011-03-20 09:40:01 +0200

    removed unused import

--- Haskeroids/Tick.hs
+++ Haskeroids/Tick.hs
@@ -1,7 +1,6 @@
 module Haskeroids.Tick where
 
 import Haskeroids.Keyboard (Keyboard)
-import Haskeroids.Geometry.Body (HasBody(..), updateBody)
 
 class Tickable t where
     tick :: Keyboard -> t -> t
