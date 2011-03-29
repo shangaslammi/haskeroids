6dece331792d4952ef462bd8ebd56ba656f1d741
Sami Hangaslammi <shang@iki.fi>
2011-03-29 18:36:01 +0300

    added a couple of comments to bullet

--- Haskeroids/Bullet.hs
+++ Haskeroids/Bullet.hs
@@ -14,10 +14,12 @@ instance LineRenderable Bullet where
     interpolatedLines f (Bullet b) = [transform b' bulletLine]
         where b' = interpolatedBody f b
 
+-- | Initialize a new bullet with the given position and direction
 initBullet :: Vec2 -> Float -> Bullet
 initBullet pos angle = Bullet $ Body (pos' /+/ vel) angle vel 0 pos' angle
     where vel  = polar bulletSpeed angle
           pos' = pos /+/ (polar 12.0 angle)
     
+-- | Update a bullet to a new position
 updateBullet :: Bullet -> Bullet
 updateBullet (Bullet b) = Bullet $ updateBody b
