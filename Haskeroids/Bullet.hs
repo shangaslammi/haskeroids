6aafeb898e2f236b506697ade604b7b74979114d
Sami Hangaslammi <shang@iki.fi>
2011-07-24 14:19:37 +0300

    cody style changes and cleanup for Bullet module

--- Haskeroids/Bullet.hs
+++ Haskeroids/Bullet.hs
@@ -1,9 +1,10 @@
 
-module Haskeroids.Bullet (
-    Bullet,
-    initBullet,
-    updateBullet,
-    bulletActive ) where
+module Haskeroids.Bullet
+    ( Bullet
+    , initBullet
+    , updateBullet
+    , bulletActive
+    ) where
 
 import Haskeroids.Render
 import Haskeroids.Collision
@@ -12,32 +13,42 @@ import Haskeroids.Geometry.Body
 
 import Control.Applicative
 
+-- | Speed of a bullet in pixels per tick
+bulletSpeed :: Float
 bulletSpeed = 10.0
+
+-- | The visual line for a bullet
+bulletLine :: LineSegment
 bulletLine  = LineSegment ((0,-bulletSpeed/2.0),(0,bulletSpeed/2.0))
+
+-- | The collision line for a bullet
+bulletLine' :: LineSegment
 bulletLine' = LineSegment ((0,-bulletSpeed/2.0),(0,bulletSpeed))
+
+-- | The maximum number of ticks that a bullet stays active
+bulletMaxLife :: Int
 bulletMaxLife = 70
 
-data Bullet = Bullet {
-    bulletLife :: Int,
-    bulletBody :: Body }
+data Bullet = Bullet
+    { bulletLife :: Int
+    , bulletBody :: Body
+    }
 
 instance LineRenderable Bullet where
-    interpolatedLines f (Bullet _ b) = [transform b' bulletLine]
-        where b' = interpolatedBody f b
+    interpolatedLines f (Bullet _ b) = [transform b' bulletLine] where
+        b' = interpolatedBody f b
 
 instance Collider Bullet where
-    collisionCenter = bodyPos . bulletBody
-    collisionRadius = const bulletSpeed
-
+    collisionCenter  = bodyPos . bulletBody
+    collisionRadius  = const bulletSpeed
     collisionLines b = return $ transform (bulletBody b) bulletLine'
-
 
 -- | Initialize a new bullet with the given position and direction
 initBullet :: Vec2 -> Float -> Bullet
-initBullet pos angle = Bullet bulletMaxLife body
-    where body = Body pos' angle vel 0 pos' angle
-          vel  = polar bulletSpeed angle
-          pos' = pos /+/ polar 12.0 angle
+initBullet pos angle = Bullet bulletMaxLife body where
+    body = Body pos' angle vel 0 pos' angle
+    vel  = polar bulletSpeed angle
+    pos' = pos /+/ polar 12.0 angle
 
 -- | Update a bullet to a new position
 updateBullet :: Bullet -> Bullet
