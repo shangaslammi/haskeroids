760ac89cfbb4d09ea4176807fa42a61a755d1eb9
Sami Hangaslammi <shang@iki.fi>
2011-07-25 19:43:23 +0300

    font module basic api

--- Haskeroids/Text/Font.hs
+++ Haskeroids/Text/Font.hs
@@ -1,2 +1,22 @@
 
-module Haskeroids.Text.Font where
+module Haskeroids.Text.Font
+    ( loadFont
+    , charLines
+    ) where
+
+import Haskeroids.Geometry
+
+import System.IO
+
+data Font = Font
+type FontSize = Float
+
+loadFont :: FilePath -> IO Font
+loadFont = fmap parseFont . readFile
+
+parseFont :: String -> Font
+parseFont s = undefined
+
+charLines :: Font -> FontSize -> Char -> [LineSegment]
+charLines = undefined
+
